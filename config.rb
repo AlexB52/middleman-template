3ee87a724202ddb16144c165b9617635337fb1e5
Alexandre Barret <barretalexandre@hotmail.com>
2019-07-07 14:31:01 +1200

    Add livereload

--- config.rb
+++ config.rb
@@ -13,6 +13,7 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+activate :livereload
 activate :directory_indexes
 activate :livereload
 activate :external_pipeline,
